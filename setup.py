53c951528f3bc2f494b96569e2c6c343244520ee
JeongUkJae <JeongUkJae@gmail.com>
2019-03-04 15:46:08 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,9 +15,10 @@ setup(
     author_email='jeongukjae@gmail.com',
     license='MIT',
     classifiers=[
-        "Development Status :: 1 - Planning", "Environment :: Console",
+        "Development Status :: 3 - Beta", "Environment :: Console",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
