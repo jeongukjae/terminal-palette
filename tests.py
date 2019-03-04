98f04bfecec85f479a56bf3f3af80c82b9351d34
JeongUkJae <JeongUkJae@gmail.com>
2019-03-04 15:47:50 +0900

    Update tests.py

--- tests.py
+++ tests.py
@@ -11,6 +11,12 @@ def test_colors():
     assert '\x1b[34mSome Tests\x1b[0m' == palette.blue('Some Tests')
 
 
+def test_rgb():
+    palette = Palette()
+    assert '\x1b[38;2;10;20;30mHello, World!\x1b[0m' == palette.rgb(
+        10, 20, 30)('Hello, World!')
+
+
 def test_nested():
     P = Palette
     assert '\x1b[31mRED!!\x1b[34mBLUE!!\x1b[0m\x1b[31mRED AGAIN!!\x1b[0m' == P(
