bedf79477ef0f175d5be7b8fcb8c7637616908b1
JeongUkJae <JeongUkJae@gmail.com>
2019-03-04 14:27:25 +0900

    Create tests.py

---
+++ tests.py
@@ -0,0 +1,17 @@
+from terminal_palette import Palette
+
+
+def test_is_palette_callable():
+    assert callable(Palette())
+
+
+def test_colors():
+    palette = Palette()
+    assert '\x1b[31mHello, World!\x1b[0m' == palette.red('Hello, World!')
+    assert '\x1b[34mSome Tests\x1b[0m' == palette.blue('Some Tests')
+
+
+def test_nested():
+    P = Palette
+    assert '\x1b[31mRED!!\x1b[34mBLUE!!\x1b[0m\x1b[31mRED AGAIN!!\x1b[0m' == P(
+    ).red('RED!!' + P().blue('BLUE!!') + 'RED AGAIN!!')
